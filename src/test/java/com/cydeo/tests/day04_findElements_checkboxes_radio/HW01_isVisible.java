e547898d5eed4decd92ab03a0a4fc5cad27d256f
AdamB25 <barsialtunit@gmail.com>
2024-02-22 15:22:16 -0500

    day 4 done

---
+++ src/test/java/com/cydeo/tests/day04_findElements_checkboxes_radio/HW01_isVisible.java
@@ -0,0 +1,30 @@
+package com.cydeo.tests.day04_findElements_checkboxes_radio;
+
+import org.junit.jupiter.api.Test;
+
+public class HW01_isVisible {
+
+    @Test
+    void test1() {
+        //1. Open Chrome browser
+
+
+
+
+        //2- Go to: https://practice.cydeo.com/abtest
+
+
+
+        //3- Locate “CYDEO” link, verify it is displayed.
+
+
+        //4- Refresh the page.
+
+
+        //5- Verify it is displayed, again.
+
+
+        //close the browser
+
+    }
+}
